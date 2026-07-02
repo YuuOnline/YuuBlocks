3ddcfa8d88d445751675f9c1558a65ab4885ec83
Alex Chandler <alex.d.chandler@gmail.com>
2026-07-02 10:59:37 -0700

    Added color array and ability to change colors. Adjusted raycast length to 3.5m

--- YuuBlocks.ts
+++ YuuBlocks.ts
@@ -2,6 +2,7 @@ import { Color } from "./Yuu API/Basic Types/Color";
 import { Quaternion } from "./Yuu API/Basic Types/Quaternion";
 import { Vector3 } from "./Yuu API/Basic Types/Vector3";
 import { inWorldConsole } from "./Yuu API/Console";
+import { Controller } from "./Yuu API/Controller";
 import { Entity } from "./Yuu API/Entity";
 import { Events } from "./Yuu API/Events";
 import { Player } from "./Yuu API/Player";
@@ -9,12 +10,13 @@ import { registerStart } from "./Yuu API/RegisterStart";
 import { spawnPrimitive } from "./Yuu API/SpawnPrimitive";
 
 
-let colorHue = Math.random();
-let color = Color.fromHSV(colorHue, 1, 1);
+let colorIndex = 0;
+// Add intermediary colors and such
+let colors: Color[] = [Color.lavender, Color.magenta, Color.pink, Color.red, Color.orange, Color.yellow, Color.green, Color.cyan, Color.blue, Color.purple, Color.black, new Color(0.5, 0.5, 0.5), Color.white];
 
 const rayCasts = {
-  leftPointer: spawnPrimitive.cube(new Vector3(0, -10, 0), Vector3.one, Quaternion.one, color, 0.5, false, 'Empty', undefined),
-  rightPointer: spawnPrimitive.cube(new Vector3(0, -10, 0), Vector3.one, Quaternion.one, color, 0.5, false, 'Empty', undefined),
+  leftPointer: spawnPrimitive.cube(new Vector3(0, -10, 0), Vector3.one, Quaternion.one, colors[colorIndex], 0.5, false, 'Empty', undefined),
+  rightPointer: spawnPrimitive.cube(new Vector3(0, -10, 0), Vector3.one, Quaternion.one, colors[colorIndex], 0.5, false, 'Empty', undefined),
 }
 
 
@@ -25,6 +27,9 @@ function start() {
   console.log('Hello World!');
 
   Events.onPhysicsUpdate(onUpdate);
+
+  Controller.subscribe('rightB', 'Pressed', updateRayColor);
+  Controller.subscribe('leftY', 'Pressed', updateRayColor);
 }
 
 
@@ -42,15 +47,14 @@ function drawRayCast(pointer: Entity, isRight: boolean) {
     const placementPos = handPos.add(handForward.multiply(2.5));
 
     pointer.pos = placementPos;
-    pointer.scale = new Vector3(0.001, 0.005, 5);
+    pointer.scale = new Vector3(0.001, 0.005, 3.5);
     pointer.rot = Quaternion.lookAt(handForward, Vector3.up);
   }
 }
 
 function updateRayColor() {
-  colorHue = Math.random();
-  color = Color.fromHSV(colorHue, 1, 1);
+  colorIndex = (colorIndex + 1) % colors.length;
 
-  rayCasts.leftPointer.mesh.color.set(color, 0.5);
-  rayCasts.rightPointer.mesh.color.set(color, 0.5);
+  rayCasts.leftPointer.mesh.color.set(colors[colorIndex], 0.5);
+  rayCasts.rightPointer.mesh.color.set(colors[colorIndex], 0.5);
 }
